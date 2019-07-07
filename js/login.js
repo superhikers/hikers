cd8e406276c55a9b3d7e5d80119e66a99c2cbf4b
superhikers <superhikers>
2019-07-07 22:19:25 +0800

    上传本地文件

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function fn(){
+	var a= document.getElementsByClassName("dom")[0];
+	var b= document.getElementsByClassName("dom1")[0];
+	var c= document.getElementsByClassName("dl")[0];
+	var d= document.getElementsByClassName("zc")[0];
+	a.style.display="block";
+	b.style.display="none";
+	c.style.backgroundColor="#ffffff";
+	d.style.backgroundColor="#E0E0E0";
+}
+function fn1(){
+	var a= document.getElementsByClassName("dom")[0];
+	var b= document.getElementsByClassName("dom1")[0];
+	var c= document.getElementsByClassName("dl")[0];
+	var d= document.getElementsByClassName("zc")[0];
+	b.style.display="block"; 
+	a.style.display="none";
+	d.style.backgroundColor="white";
+	c.style.backgroundColor="#E0E0E0";
+}
